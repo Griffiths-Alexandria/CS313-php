9d80112d5805e1cf31dc59b01c097cdbb0a5c3eb
Griffiths-Alexandria <alexcgriff@gmail.com>
2016-01-17 12:10:20 -0800

    update

--- assignments/about.php
+++ assignments/about.php
@@ -21,12 +21,12 @@
         <p class="paragraph">
             Hi everyone! My name is Alex and I live in a beautiful city in North San Diego
 county. I have lived and traveled all around the world. I am a Senior majoring in Web Design 
-and Development. I am in my final 2 semesters of school so I am extremely excited to see the light at the end of this tunnel (and "senioritis" is definitely kicking in)! Along with my studies, I also do Portrait Photography, a Graphic Designer for a small family-run Etsy company, and the Laurel advisor in my ward. I enjoy anything creative and artistic.
+and Development. <br> I am in my final 2 semesters of school so I am extremely excited to see the light at the end of this tunnel (and "senioritis" is definitely kicking in)! Along with my studies, I also do Portrait Photography, a Graphic Designer for a small family-run Etsy company, and the Laurel advisor in my ward. I enjoy anything creative and artistic.
 I love spending the [summer] days at the beach with my kids and having beach picnics with my brother
-and sister-in-law. We also love Disneyland, WonderCon, and Comic-Con. I would have to say that fresh 
+and sister-in-law. We also love Disneyland, WonderCon, and Comic-Con. <br> I would have to say that fresh 
 sashimi, sushi, and thai spring rolls are my favorite things to eat, but I do love any great food
 and am willing to try almost any type of cuisine.
- 
+<br>
 BYU-I has provided such a wonderful opportunity for me to finish my degree online, 
 while still being able to work, and be a mom. I completed 3 years at BYU, but dropped out 
 when I got married and I never finished my degree. Transferring to the UC school system in 
