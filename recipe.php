c6c8d7b5b5000ab3b8828f45985e9a6169757039
Griffiths-Alexandria <alexcgriff@gmail.com>
2016-02-07 12:17:55 -0800

    lets try again

--- recipe.php
+++ recipe.php
@@ -7,35 +7,37 @@
     <body>
         <div>
 
-            <h1>Delicous Recipes</h1>
+            <h1>Delicious Recipes</h1>
 
-            <?php
-            $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');                                       //Get host from OpenShift
-            $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');                                       //Get Port from OpenShift
-            $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');                                   //Get UserName from OpenShift
-            $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');                               //Get Password from OpenShift
-            $dbName = "recipe_db";                                                              //Assign a static Database name
-            $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);  //create a secure database variable
-            try {
-                // Create the PDO connection
-                $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+            <?php include '/recipedb.php'; 
+          
+try
+{
+	// Create the PDO connection
+	$db = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 
-                // prepare the statement
-                $statement = $db->prepare('SELECT authorID, recipeID, meal_typeID, ingredientID FROM food_recipe');
-                $statement->execute();
+	// prepare the statement
+	$statement = $db->prepare('SELECT authorID, recipeID, meal_typeID, ingredientID FROM food_recipe');
+	$statement->execute();
 
-                // Go through each result
-                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                    echo '<p>';
-                    echo '<strong>' . $row['recipeID'] . ' ' . $row['authorID'] . ':';
-                    echo $row['indredientID'] . '</strong>' . ' - ' . $row['meal_typeID'];
-                    echo '</p>';
-                }
-            } catch (PDOException $ex) {
-                echo "Error connecting to DB. Details: $ex";
-                die();
-            }
-            ?>
+	// Go through each result
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
+		echo '<p>';
+		echo '<strong>' . $row['authorID'] . ' ' . $row['recipeID'] . ':';
+		echo $row['ingredientID'] . '</strong>' . ' - ' . $row['meal_typeID'];
+		echo '</p>';
+	}
+
+}
+catch (PDOException $ex)
+{
+	echo "Error connecting to DB. Details: $ex";
+	die();
+}
+
+?>
+
 
         </div>
 
---
+++ recipedb.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbhost = 'localhost';
+$dbuser = 'adminlyaXSvD';
+$dbpass = 'bTytkAuukZCc';
+$dbname = 'recipe_db';
+
+$conn = mysql_connect($dbhost,$dbname, $dbpass, $dbuser);
+
+?>
