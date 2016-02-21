39403611172df8d77c5369ff0b286d8992d07ac6
Griffiths-Alexandria <alexcgriff@gmail.com>
2016-02-21 14:41:59 -0800

    PLEASE

--- displayrecipes.php
+++ displayrecipes.php
@@ -20,44 +20,38 @@
             
             <h1>Our Recipes</h1>
 
+<?php
+try {
+    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');                                       //Get host from OpenShift
+    $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');                                       //Get Port from OpenShift
+    $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');                                   //Get UserName from OpenShift
+    $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');                               //Get Password from OpenShift
+    $dbName = "recipes_db";                                                              //Assign a static Database name
+    $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);  //create a secure database variable
+    // prepare the statement
+    $statement = $db->prepare('SELECT recipe.recipeNAME , recipe.recipeDESC, ingredient.ingredientNAME, ingredient.ingredientMEAS
+FROM recipeingredient
+INNER JOIN recipe
+ON recipeingredient.recipeID=recipe.recipeID 
+INNER JOIN ingredient
+ON recipeingredient.ingredientID=ingredient.ingredientID');
+    $statement->execute();
 
-<?php 
-             try
-    {
-      $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');                                       //Get host from OpenShift
-      $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');                                       //Get Port from OpenShift
-      $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');                                   //Get UserName from OpenShift
-      $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');                               //Get Password from OpenShift
-	  $dbName = "recipes_db";                                                              //Assign a static Database name
-	  $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);  //create a secure database variable
-           
+    // Go through each result
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC )) {
 
-	// prepare the statement
-	$statement = $db->prepare("SELECT recipeNAME, recipeDESC FROM recipe");
-	$statement->execute();
-
-        $statements = $db->prepare("SELECT ingredientMEAS , ingredientNAME FROM ingredient INNER JOIN recipe WHERE recipeNAME=$row[recipeNAME]");
-	$statements->execute();
-        
-	// Go through each result
-	while ($row = $statement->fetch(PDO::FETCH_ASSOC) && $row2 = $statements->fetch(PDO::FETCH_ASSOC))
-	{
-		echo '<h2>';
-		echo '<strong>' . $row['recipeNAME'];
-		echo '</h2> <p>';
-                echo '<strong>' . $rows['ingredientNAME'] . ' ' . $rows['ingredientMEAS'];
-		echo '</p> <p>';
-                echo $row['recipeDESC'];
-		echo '</p>';
-	}
-
+        echo '<h2>';
+        echo '<strong>' . (array_unique($row)['recipeNAME']);
+        echo '</h2> <p>';
+        echo '<strong>' . $row['ingredientMEAS'] . ' ' . $row['ingredientNAME'];
+        echo '</p> <p>';
+        echo (array_unique($row)['recipeDESC']);
+        echo '</p>';
+    }
+} catch (PDOException $ex) {
+    echo "Error connecting to DB. Details: $ex";
+    die();
 }
-catch (PDOException $ex)
-{
-	echo "Error connecting to DB. Details: $ex";
-	die();
-}
-
 ?>
 
         </div>
