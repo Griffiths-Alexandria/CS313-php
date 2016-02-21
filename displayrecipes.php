8f11114cd3cfe26896eb912ed3a74a8700a04c71
Griffiths-Alexandria <alexcgriff@gmail.com>
2016-02-21 13:49:16 -0800

    please

--- displayrecipes.php
+++ displayrecipes.php
@@ -20,36 +20,44 @@
             
             <h1>Our Recipes</h1>
 
-<?php
-try {
-    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');                                       //Get host from OpenShift
-    $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');                                       //Get Port from OpenShift
-    $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');                                   //Get UserName from OpenShift
-    $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');                               //Get Password from OpenShift
-    $dbName = "recipes_db";                                                              //Assign a static Database name
-    $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);  //create a secure database variable
-    // prepare the statement
-    $statement = $db->prepare('SELECT recipe.recipeNAME, recipe.recipeDESC, ingredient.ingredientNAME, ingredient.ingredientMEAS
-FROM recipe
-INNER JOIN ingredient
-ON recipe.recipeID=ingredient.recipeID 
-GROUP BY recipe.recipeID');
-    $statement->execute();
 
-    // Go through each result
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        echo '<h2>';
-        echo '<strong>' . $row['recipeNAME'];
-        echo '</h2> <p>';
-        echo '<strong>' . $row['ingredientMEAS'] . ' ' . $row['ingredientNAME'];
-        echo '</p> <p>';
-        echo $row['recipeDESC'];
-        echo '</p>';
-    }
-} catch (PDOException $ex) {
-    echo "Error connecting to DB. Details: $ex";
-    die();
+<?php 
+             try
+    {
+      $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');                                       //Get host from OpenShift
+      $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');                                       //Get Port from OpenShift
+      $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');                                   //Get UserName from OpenShift
+      $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');                               //Get Password from OpenShift
+	  $dbName = "recipes_db";                                                              //Assign a static Database name
+	  $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);  //create a secure database variable
+           
+
+	// prepare the statement
+	$statement = $db->prepare("SELECT recipeNAME, recipeDESC FROM recipe");
+	$statement->execute();
+
+        $statements = $db->prepare("SELECT ingredientMEAS , ingredientNAME FROM ingredient INNER JOIN recipe WHERE recipeNAME=$row[recipeNAME]");
+	$statements->execute();
+        
+	// Go through each result
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC) && $row2 = $statements->fetch(PDO::FETCH_ASSOC))
+	{
+		echo '<h2>';
+		echo '<strong>' . $row['recipeNAME'];
+		echo '</h2> <p>';
+                echo '<strong>' . $rows['ingredientNAME'] . ' ' . $rows['ingredientMEAS'];
+		echo '</p> <p>';
+                echo $row['recipeDESC'];
+		echo '</p>';
+	}
+
 }
+catch (PDOException $ex)
+{
+	echo "Error connecting to DB. Details: $ex";
+	die();
+}
+
 ?>
 
         </div>
