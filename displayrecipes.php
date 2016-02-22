a47f4ac51f21ac27cf1aabb2ce2cff5368f2d4ef
Griffiths-Alexandria <alexcgriff@gmail.com>
2016-02-21 16:18:18 -0800

    no

--- displayrecipes.php
+++ displayrecipes.php
@@ -39,7 +39,7 @@ ON recipeingredient.ingredientID=ingredient.ingredientID
                 $statement->execute();
 
                 // Go through each result
-                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                while ($row = $statement->fetch(PDO::FETCH_NAMED)) {
                    
                     $recipeDESC = $row['recipeDESC'];
                     $ingredientMEAS = $row['ingredientMEAS'];
